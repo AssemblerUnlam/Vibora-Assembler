633be813e658d29a5973214ee37d1e5e646f67f5
fns92 <sanchez36822171@gmail.com>
2018-11-29 12:12:09 -0300

    uso de before

--- src/test/Tests.java
+++ src/test/Tests.java
@@ -1,45 +1,47 @@
 package test;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import entidades.Vibora;
-import junit.framework.Assert;
 import utilidades.Direccion;
 
 public class Tests {
 
+	Vibora vibora;
+	
+	@Before 
+	public void inicializar() {
+	       vibora = new Vibora();
+	    }
+	
 	@Test
 	public void inicializaVibora3Puntos() {
-		Vibora vibora = new Vibora();
 		Assert.assertEquals(3, vibora.getCuerpo().size());
 	}
 	
 	@Test
 	public void comienzaSentidoDerecha() {
-		Vibora vibora = new Vibora();
 		Assert.assertEquals("DERECHA", vibora.getDireccion().toString());;
 	}
 	
 	public void seMueveAIzquieda() {
-		Vibora vibora = new Vibora();
 		vibora.setDireccion(Direccion.IZQUIERDA);
 		Assert.assertEquals("IZQUIERDA", vibora.getDireccion().toString());;
 	}
 	
 	public void seMueveAArriba() {
-		Vibora vibora = new Vibora();
 		vibora.setDireccion(Direccion.ARRIBA);
 		Assert.assertEquals("ARRIBA", vibora.getDireccion().toString());;
 	}
 	
 	public void seMueveAAbajo() {
-		Vibora vibora = new Vibora();
 		vibora.setDireccion(Direccion.ABAJO);
 		Assert.assertEquals("ABAJO", vibora.getDireccion().toString());;
 	}
 	
 	public void seMueveADerecha() {
-		Vibora vibora = new Vibora();
 		vibora.setDireccion(Direccion.ABAJO);
 		vibora.setDireccion(Direccion.DERECHA);
 		Assert.assertEquals("DERECHA", vibora.getDireccion().toString());;
